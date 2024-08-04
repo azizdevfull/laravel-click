56106f737173ddac6db1c8e653b5ea6eca718dc6
Azizbek Isroilov <aziz16110904@gmail.com>
2024-08-04 14:54:13 +0500

    completed

---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Order;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Order::create([
+            'total' => '2000',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
